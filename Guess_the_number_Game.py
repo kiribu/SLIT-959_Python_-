a1040f5a5e82d00fb8db559b4827412fa86583b9
kiribu <118684266+kiribu@users.noreply.github.com>
2024-09-25 17:18:59 +0400

    Add files via upload

---
+++ Guess_the_number_Game.py
@@ -0,0 +1,26 @@
+import random
+
+def game():
+    # Загадываем случайное число
+    hidden_number = random.randint(1, 100)
+
+    print(f"Я загадал целое число от 1 до 100.")
+    attempts = 0
+
+    while True:
+        # Вариант пользователя
+        response = int(input(f"Угадайте число: "))
+        attempts += 1
+
+        if response == hidden_number:
+            # Угадал число
+            print(f"Вы угадали число! Потребовалось {attempts} попыток.")
+            break
+        elif response < hidden_number:
+            # Число меньше загаданного
+            print(f"Загаданное число больше {response}")
+        else:
+            # Число больше загаданного
+            print(f"Загаданное число меньше {response}")
+
+game()
